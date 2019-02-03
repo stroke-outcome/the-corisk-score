ecda872b266f2d6754025abe10cbf8dc584009c4
Victor Parmar <vic@smalldata.tech>
2019-02-03 19:15:20 +0100

    Updates

---
+++ precache-manifest.e79b53cf3269d55d01da9e2028b444d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75dfd91a92d5cf7374ab",
+    "url": "/the-corisk-score/static/js/runtime~main.75dfd91a.js"
+  },
+  {
+    "revision": "1c7b1feca8edf198c6b0",
+    "url": "/the-corisk-score/static/js/main.1c7b1fec.chunk.js"
+  },
+  {
+    "revision": "b6618fb18924f1fda7fc",
+    "url": "/the-corisk-score/static/js/1.b6618fb1.chunk.js"
+  },
+  {
+    "revision": "1c7b1feca8edf198c6b0",
+    "url": "/the-corisk-score/static/css/main.36615b39.chunk.css"
+  },
+  {
+    "revision": "b6618fb18924f1fda7fc",
+    "url": "/the-corisk-score/static/css/1.e07da603.chunk.css"
+  },
+  {
+    "revision": "6a2fdade586d2b208ab9b26748075805",
+    "url": "/the-corisk-score/index.html"
+  }
+];
